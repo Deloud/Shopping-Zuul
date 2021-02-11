e8d0fe4e454cd739fba4ce9bbf861853b6e4ef1a
karice <kl529@naver.com>
2021-02-11 15:57:27 +0900

    중간점검

---
+++ src/test/java/com/shopping/shoppingmallzuul/ShoppingMallZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.shoppingmallzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
